f1584fc3069a128bf46a581d96b9e847454d5d20
Hinkmond Wong <hinkmond_wong@apple.com>
2022-12-21 23:25:45 -0800

    Added full compatibility with legacy version.

--- kafka-direct-iotmsg.py
+++ kafka-direct-iotmsg.py
@@ -25,24 +25,48 @@ from operator import add
 
 
 def print_row(row):
-    jsonLine = row.__getitem__('value')
-    if re.search(r"temperature.*", jsonLine):
-       tempLine = re.sub(r"\"temperature\":", "", jsonLine).split(',')[0]
-       print(jsonLine)
-       print(tempLine)
-    elif re.search(r"humidity.*", jsonLine):
-       humidityLine = re.sub(r"\"humidity\":", "", jsonLine).split(',')[0]
-       print(humidityLine)
+    json_line = row.__getitem__('value')
+    if re.search(r"temperature.*", json_line):
+       temp_line = re.sub(r"\"temperature\":", "", json_line).split(',')[0]
+       print(json_line)
+       print(temp_line)
+    elif re.search(r"humidity.*", json_line):
+       humidity_line = re.sub(r"\"humidity\":", "", json_line).split(',')[0]
+       print(json_line)
+       print(humidity_line)
 
 def process_json(batch_df, batch_id):
     json_raw_str = batch_df.rdd \
-      .map(lambda r: " ".join(r) + " ") \
+      .map(lambda row: " ".join(row) + " ") \
       .aggregate("", lambda a,b: a+b, lambda a,b: a+b)
     json_line = re.sub(r"\s+", "", json_raw_str, flags=re.UNICODE)
     print(json_line)
     json_df = spark.read.json(sc.parallelize([json_line]))
     json_df.printSchema()
     json_df.show(truncate=False)
+
+def process_batch_values(batch_df, batch_id):
+    # Match local function variables to global variables
+    global TEMP_TOTAL
+    global TEMP_COUNT
+    global TEMP_AVG
+
+    # Search for specific IoT data values (assumes json_rows are split(','))
+    json_rows = batch_df.rdd
+    temp_values = json_rows.filter(lambda row: re.findall(r"temperature.*", row.__getitem__('value'), 0))
+
+    # Parse out just the value without the JSON key
+    parsed_temp_values = temp_values.map(lambda row: re.sub(r"\"temperature\":", "", row.__getitem__('value')) \
+      .split(',')[0])
+
+    temps_list = parsed_temp_values.collect()
+    for temp_float in temps_list:
+      TEMP_TOTAL += float(temp_float)
+      TEMP_COUNT += 1
+      TEMP_AVG = TEMP_TOTAL / TEMP_COUNT
+    print("Temperature Total = " + str(TEMP_TOTAL))
+    print("Temperature Count = " + str(TEMP_COUNT))
+    print("Avg Temperature = " + str(TEMP_AVG))
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -52,9 +76,9 @@ if __name__ == "__main__":
     ###############
     # Globals
     ###############
-    tempTotal = 0.0
-    tempCount = 0
-    tempAvg = 0.0
+    TEMP_TOTAL = 0.0
+    TEMP_COUNT = 0
+    TEMP_AVG = 0.0
 
     brokers, topic = sys.argv[1:]
     spark = SparkSession.builder.master("local[*]") \
@@ -82,8 +106,7 @@ if __name__ == "__main__":
 
     print(">>> Starting DataWriteStream of JSON lines in one batch ")
     query2 = df_to_strings.writeStream \
-     .foreachBatch(process_json) \
+     .foreachBatch(process_batch_values) \
      .start()
 
     spark.streams.awaitAnyTermination()
-
