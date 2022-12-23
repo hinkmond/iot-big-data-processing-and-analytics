0daf4eeece088116caa7d3455dfd249c4ec791bd
Hinkmond Wong <hinkmond_wong@apple.com>
2022-12-22 22:52:20 -0800

    Set log level to only errors and restrict local cluster.

--- network_wordcount.py
+++ network_wordcount.py
@@ -2,7 +2,8 @@ from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 
 # Create a local StreamingContext with two working thread and batch interval of 1 second
-sc = SparkContext("local[2]", "NetworkWordCount")
+sc = SparkContext("local[*]", "NetworkWordCount")
+sc.setLogLevel("ERROR")
 ssc = StreamingContext(sc, 1)
 
 # Create a DStream that will connect to hostname:port, like localhost:9999
