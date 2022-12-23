ef8d1adfd63e5af02a2641d2eddc0010d2721f5a
Hinkmond Wong <hinkmond_wong@apple.com>
2022-12-22 21:43:05 -0800

    Convert kafka-direct-iot-sql.py to Spark 3.3.1 compatible.

--- kafka-direct-iot-sql.py
+++ kafka-direct-iot-sql.py
@@ -8,91 +8,106 @@
       kafka-direct-iot-sql.py \
       localhost:9092 test`
 """
-from __future__ import print_function
-
 import sys
 import re
 import json
 
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
-from pyspark.streaming import StreamingContext
-from pyspark.streaming.kafka import KafkaUtils
-from pyspark.streaming.kafka import OffsetRange
+from pyspark.sql import SparkSession
+
 from operator import add
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: direct_kafka_wordcount.py <broker_list> <topic>", file=sys.stderr)
+        print("Usage: kafka-direct-iot-sql.py <broker_list> <topic>", file=sys.stderr)
         exit(-1)
 
-    sc = SparkContext(appName="PythonStreamingDirectKafkaWordCount")
-    ssc = StreamingContext(sc, 2)
-    sqlContext = SQLContext(sc)
-    sc.setLogLevel("WARN")
+    brokers, topic = sys.argv[1:]
+    spark = SparkSession.builder.master("local[*]") \
+                    .appName('kafka-direct-iotmsg') \
+                    .getOrCreate()
+    sc = spark.sparkContext
+    sc.setLogLevel("ERROR")
 
     ##############
     # Globals
     ##############
-    globals()['maxTemp'] = sc.accumulator(0.0)
+    globals()['max_temp'] = sc.accumulator(0.0)
 
     brokers, topic = sys.argv[1:]
-    kvs = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
-    jsonDStream = kvs.map(lambda (key, value): value)
+    print(">>> Starting spark, bootstrap.servers: " + brokers + ", topic: " + topic + " ...")
+    df = spark \
+     .readStream \
+     .format("kafka") \
+     .option("kafka.bootstrap.servers", brokers) \
+     .option("subscribe", topic) \
+     .option("startingOffsets", "latest") \
+     .load()
 
     # Define function to process RDDs of the json DStream to convert them
     #   to DataFrame and run SQL queries
-    def process(time, rdd):
+    def process(batch_df, batch_id):
         # Match local function variables to global variables
-        maxTemp = globals()['maxTemp']
+        max_temp = globals()['max_temp']
 
-        print("========= %s =========" % str(time))
+        rdd = batch_df.rdd
+        print("========= %s =========" % str(batch_id))
         print("rdd = %s" % str(rdd))
 
         try:
           if not rdd.isEmpty():
             # Parse the multiple JSON lines from the DStream RDD and trim any extra spaces
-            jsonLinesRDD = rdd.map(lambda x: re.sub(r"\s+", "", x, flags=re.UNICODE)).reduce(add)
-            print("jsonLinesRDD = %s" % str(jsonLinesRDD))
+            json_raw_lines = rdd \
+              .map(lambda row: " ".join(row) + " ") \
+              .aggregate("", lambda a,b: a+b, lambda a,b: a+b)
+            json_processed_lines = re.sub(r"\s+", "", json_raw_lines, flags=re.UNICODE)
+            print("json_processed_lines = %s" % str(json_processed_lines))
 
             # Convert RDD of the List of multiple JSON lines to Spark SQL Context by first
             #    joining the list of mulitple JSON lines in a new RDD with a single JSON line
-            jsonRDD = sqlContext.read.json(sc.parallelize([jsonLinesRDD]))
+            json_rdd = spark.read.json(sc.parallelize([json_processed_lines]))
 
             # Register the JSON SQL Context as a temporary SQL Table
             print("JSON Schema\n=====")
-            jsonRDD.printSchema()
-            jsonRDD.registerTempTable("iotmsgsTable")
+            json_rdd.printSchema()
+            json_rdd.createOrReplaceTempView("iotmsgs_table")
 
             #############
             # Processing and Analytics go here
             #############
 
-            sqlContext.sql("select payload.data.* from iotmsgsTable order by temperature desc").show(n=100)
+            spark.sql("select payload.data.* from iotmsgs_table order by temperature desc").show(n=100)
 
             # Sort
-            sqlContext.sql("select payload.data.temperature from iotmsgsTable order by temperature desc").show(n=100)
+            spark.sql("select payload.data.temperature from iotmsgs_table order by temperature desc").show(n=100)
 
-            currentTemp = sqlContext.sql("select payload.data.temperature from iotmsgsTable order by temperature desc").collect()[0].temperature
-            print("Current temp = " + str(currentTemp))
-            if (currentTemp > maxTemp.value):
-              maxTemp.value = currentTemp
-            print("Max temp = " + str(maxTemp.value))
+            current_temp = spark.sql("select payload.data.temperature from iotmsgs_table order by temperature desc").collect()[0].temperature
+            print("Current temp = " + str(current_temp))
+            if (current_temp > max_temp.value):
+              max_temp.value = current_temp
+            print("Max temp = " + str(max_temp.value))
 
             # Search
-            sqlContext.sql("select payload.data.temperature from iotmsgsTable where payload.data.temperature = 100.0").show(n=100)
+            spark.sql("select payload.data.temperature from iotmsgs_table where payload.data.temperature = 100.0").show(n=100)
 
             # Filter
-            sqlContext.sql("select payload.data.temperature from iotmsgsTable where payload.data.temperature > 85").show(n=100)
+            spark.sql("select payload.data.temperature from iotmsgs_table where payload.data.temperature > 85").show(n=100)
 
             # Clean-up
-            sqlContext.dropTempTable("iotmsgsTable")
+            spark.dropTempTable("iotmsgs_table")
         # Catch any exceptions
         except:
             pass
 
-    # Process each RDD of the DStream coming in from Kafka
-    jsonDStream.foreachRDD(process)
+    print(">>> Starting DataFrame Select value Processing")
+    df_to_strings = df.selectExpr("CAST(value AS STRING)")
 
-    ssc.start()
-    ssc.awaitTermination()
+    print(">>> Starting DataWriteStream of Value column as Strings")
+    query1 = df_to_strings.writeStream \
+     .foreachBatch(process) \
+     .start()
+   
+    spark.streams.awaitAnyTermination()
+
