1a7c373b033e0d2d2080a83e61becf6e63f9ee9c
Hinkmond Wong <hinkmond_wong@apple.com>
2025-06-06 21:20:14 -0700

    Updated to latest mods.

--- checkkafkaserver.sh
+++ checkkafkaserver.sh
@@ -2,13 +2,26 @@
 
 export TZ="US/Pacific"; date
 
-kafka_connection="`nc  -v -w 1 -i 1 18.236.157.126 22 2>&1 | grep Connected`"
+kafka_connection="`nc  -v -w 1 -i 1 35.86.119.218 9092 2>&1 | grep Connected`"
 if [ "$kafka_connection" == "" ]; then
   pkill -9 java 
   pkill -9 python
   echo "Killed servers"
 else
   echo "kafka connection OK"
+fi
+
+kafka_processing="`cd ~/kafka_[0-9].*[0-9]/; bin/kafka-console-consumer.sh --timeout-ms 2500 --bootstrap-server 35.86.119.218:9092 --topic iotmsgs --from-beginning 2>&1 | grep -v ERROR | grep -v Timeout | grep Processed |  tr -s ' ' | cut -f5 -d' '`"
+if [ "$kafka_processing" == "0" ]; then
+  kafka_pid="`ps -ef | grep java | grep 'server.properties' | tr -s ' ' | tail -1 | cut -f2 -d' '`"
+  if [ "$kafka_pid" != "" ]; then
+    kill -9 $kafka_pid
+    echo "Killed kafka processing PID: $kafka_pid"
+  else
+    echo "No kafka processing due to no kafka running"
+  fi
+else
+  echo "kafka processing OK"
 fi
 
 zookeeper_running="`ps -ef | grep java | grep zookeeper.properties | tr -s ' ' | cut -f3 -d' ' | xargs`"
@@ -30,5 +43,4 @@ if [ "$kafka_running" == "" ]; then
 else
   echo "kafka already running"
 fi
-
 
